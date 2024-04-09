b0715b10da2c841ec6ec7b1c9b4bd9609c5d5535
khalil <khalil@illusioncolors.com>
2024-04-09 16:46:52 +0800

    update for precompiled action

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule AssetTextureRender.MixProject do
   use Mix.Project
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/KhalilWang/texture_render_nif"
 
   def project do
