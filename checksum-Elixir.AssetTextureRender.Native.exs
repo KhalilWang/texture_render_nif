e8b33445e277a57434b348ad86382f92909aec70
khalil <khalil@illusioncolors.com>
2024-04-09 17:18:01 +0800

    finish precompiled

---
+++ checksum-Elixir.AssetTextureRender.Native.exs
@@ -0,0 +1,11 @@
+%{
+  "libtexture_render-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:d18a99f0c0f52a1b895502538075f749cdc35c6de2893940a0e029c0b2a6f201",
+  "libtexture_render-v0.1.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ba593141d3bb7263543994b59f8232f4047881a575ba7950a87471d6b3e7f24b",
+  "libtexture_render-v0.1.2-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:59768abc9e4a98f7241ea2e2f0e044c8adeff8adf646317211b2da9c3607a4cc",
+  "libtexture_render-v0.1.2-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:03bc85b780c5262fd3b7f223710c1be6cdb5111206e829ed8b7468d76e593637",
+  "libtexture_render-v0.1.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:dc975127b627b9c2db6c248438be7dc79d348e3974becd965b0ec97967dd9856",
+  "libtexture_render-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:2f2a3687e1898a00b3322304d267ebf177ac9143224209c451d5eb6434575152",
+  "libtexture_render-v0.1.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:09b023b45e0ce4551dd54bece49da6440e7981a9c21cc6dd65bdf649392c315c",
+  "texture_render-v0.1.2-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:f0f523bab98260bf479d9dd34743a5a92887a9f485c493c8899d0280031a6a57",
+  "texture_render-v0.1.2-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:d067c4f61c6b80842b7bafb9f6bb106fb466384d731437ba0b7cc4e8aae89eed",
+}
